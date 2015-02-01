42abcb77963d727e13b0784f4161f7eb47e98978
Troy Lima <troy468@yahoo.com>
2015-02-01 14:39:41 -0500

    styling changes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,7 +6,11 @@ class CalendarsController < ApplicationController
   def new
     @calendar = Calendar.new
   end
-
+#Defining CRUD code in the Calendars controller.
+#The controller is responsible for accepting requests from the user, gathering 
+#the necessary data from the model, and rendering the correct view.
+#In rails the controller is a Ruby class with Methods that correspond to 
+#various actions in the application. 
   def create
     @calendar = Calendar.create calendar_params
     if @calendar.save
@@ -17,6 +21,8 @@ class CalendarsController < ApplicationController
       render :new
     end
   end
+  #def create allows for the creation of calendar data and displays a message depending
+  #on whether the data was saved or not.
 
   def show
     @calendar = Calendar.find params[:id]
@@ -37,8 +43,12 @@ class CalendarsController < ApplicationController
     @calendar = Calendar.find params[:id]
     @calendar.destroy
     redirect_to root_path
+    #Show,edit,update and destroy allow for the showing, editing, updating and deletion
+    #of data input into the calendar form. Parameters are identified by the assigned
+    #id in the database.
   end
-
+#The private before the def calendar_params calls for strong parameters which is a
+#precaution that helps to prevent malicious attackers from compromising data.
   private
   def calendar_params
     params.require(:calendar).permit(
